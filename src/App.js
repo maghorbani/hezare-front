d4357c21f94bcd1a9876ced8a0b1f88c767b1d09
Mohammad Ali <maghorbani759@gmail.com>
2021-03-23 02:15:59 +0430

    [IMP] routing functionality without full page refresh

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
 import React, { Component } from "react";
 import Main from "./pages/Main";
 import Results from "./pages/Results";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 export default class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			q: "",
-			local_q: "",
-		};
-	}
-	componentDidMount() {
-		const url = new URL(window.location.href);
-		const q = url.searchParams.get("q");
-		this.setState({ q, local_q: q });
-	}
+  history = createBrowserHistory();
+  constructor(props) {
+    super(props);
+    this.state = {
+      q: "",
+      local_q: "",
+    };
+  }
+  componentDidMount() {
+    const url = new URL(window.location.href);
+    const q = url.searchParams.get("q");
+    this.setState({ q, local_q: q });
+    if (q === null) {
+      this.history.push("/");
+    }
+  }
 
-	onChangeInput = (newText) => {
-		this.setState({ local_q: newText });
-	};
+  onChangeInput = (newText) => {
+    this.setState({ local_q: newText });
+  };
 
-	render() {
-		return (
-			<Router>
-				<Switch>
-					<Route exact path="/">
-						<Main />
-					</Route>
-					<Route path="/search">
-						<Main
-							vJustify="start"
-							q={this.state.q}
-							onChangeInput={this.onChangeInput}
-						/>
-						<div className="flex flex-col justify-start h-full items-center m-auto w-8/12">
-							<div className="mt-3 self-start">Results:</div>
-							<div className="w-full border h-0 mb-1"></div>
-							<Results />
-						</div>
-					</Route>
-				</Switch>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router history={this.history}>
+        <Switch>
+          <Route exact path="/">
+            <Main history={this.history} onChangeInput={this.onChangeInput} />
+          </Route>
+          <Route path="/search">
+            <Main
+              history={this.history}
+              vJustify="start"
+              q={this.state.q}
+              onChangeInput={this.onChangeInput}
+            />
+            <div className="flex flex-col justify-start h-full items-center m-auto w-8/12">
+              <div className="mt-3 self-start">Results:</div>
+              <div className="w-full border h-0 mb-1"></div>
+              <Results />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,59 +2,71 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export default class Main extends Component {
-	state = {
-		q: "",
-	};
-	static propTypes = {
-		vJustify: PropTypes.string,
-		q: PropTypes.string,
-		onChangeInput: PropTypes.func
-	};
-	static defaultProps = {
-		vJustify: "center",
-		q: "",
-	};
+  state = {
+    q: "",
+  };
+  static propTypes = {
+    vJustify: PropTypes.string,
+    q: PropTypes.string,
+    onChangeInput: PropTypes.func,
+    history: PropTypes.object,
+  };
+  static defaultProps = {
+    vJustify: "center",
+    q: "",
+  };
 
-	componentDidUpdate(prevProps, prevState) {
-		if (prevProps.q !== this.props.q) {
-			this.setState({ q: this.props.q });
-		}
-	}
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.q !== this.props.q && this.props.q !== null) {
+      this.setState({ q: this.props.q });
+    }
+  }
 
-	onChange = (e) => {
-		const q = e.target.value;
-		this.setState({ q });
-		if(this.props.onChangeInput !== undefined)
-			this.props.onChangeInput(q)
-	};
+  onChange = (e) => {
+    const q = e.target.value;
+    this.setState({ q });
+    if (this.props.onChangeInput !== undefined) this.props.onChangeInput(q);
+  };
 
-	render() {
-		const { vJustify } = this.props;
-		const { q } = this.state;
-		const height = (vJustify === "center")?"h-screen":""
-		return (
-			<div
-				className={`pt-5 flex flex-col w-8/12 justify-${vJustify} items-center m-auto ${height}`}
-			>
-				<div className="text-3xl mb-2">
-					<h1>Hezare</h1>
-				</div>
-				<div className="self-start">
-					Search Engine | Free | Independent | Open Source
-				</div>
-				<div className="w-full mt-1">
-					<form action="search">
-						<input
-							value={q}
-							className="w-full h-10 p-2 rounded-md shadow bg-white"
-							type="text"
-							placeholder="Search..."
-							name="q"
-							onChange={this.onChange}
-						/>
-					</form>
-				</div>
-			</div>
-		);
-	}
+  onSubmit = (e) => {
+    e.preventDefault();
+    /*    window.history.replaceState(
+      null,
+      "results for " + this.state.q,
+      "search?q=" + this.state.q
+	);*/
+    if (this.state.q === "") {
+      this.props.history.push("/");
+    } else this.props.history.push("/search?q=" + this.state.q);
+  };
+
+  render() {
+    const { vJustify } = this.props;
+    const { q } = this.state;
+    const height = vJustify === "center" ? "h-screen" : "";
+    return (
+      <div
+        className={`pt-5 flex flex-col w-8/12 justify-${vJustify} items-center m-auto ${height}`}
+      >
+        <div className="text-3xl mb-2">
+          <h1>Hezare</h1>
+        </div>
+        <div className="self-start">
+          Search Engine | Free | Independent | Open Source
+        </div>
+        <div className="w-full mt-1">
+          <form action="search" onSubmit={this.onSubmit}>
+            <input
+              value={q}
+              className="w-full h-10 p-2 rounded-md shadow bg-white"
+              type="text"
+              placeholder="Search..."
+              name="q"
+              onChange={this.onChange}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
